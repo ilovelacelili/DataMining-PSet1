67b87a9df751f9b30938c6fa2202b8c0df930ecb
ilovelacelili <mvivanco@estud.usfq.edu.ec>
2026-01-30 18:07:51 -0500

    new file:   db-data/base/13532/pg_internal.init 	modified:   db-data/base/16384/1247 	modified:   db-data/base/16384/1249 	modified:   db-data/base/16384/1249_fsm 	modified:   db-data/base/16384/1249_vm 	modified:   db-data/base/16384/1259 	modified:   db-data/base/16384/2608 	modified:   db-data/base/16384/2615 	modified:   db-data/base/16384/2658 	modified:   db-data/base/16384/2659 	modified:   db-data/base/16384/2662 	modified:   db-data/base/16384/2663 	modified:   db-data/base/16384/2673 	modified:   db-data/base/16384/2674 	modified:   db-data/base/16384/2685 	modified:   db-data/base/16384/2703 	modified:   db-data/base/16384/2704 	modified:   db-data/base/16384/3455 	new file:   db-data/base/16384/pg_internal.init 	modified:   db-data/global/pg_control 	new file:   db-data/global/pg_internal.init 	new file:   db-data/pg_stat_tmp/db_0.stat 	new file:   db-data/pg_stat_tmp/db_13532.stat 	new file:   db-data/pg_stat_tmp/db_16384.stat 	modified:   db-data/pg_stat_tmp/global.stat 	modified:   db-data/pg_wal/000000010000000000000001 	modified:   db-data/pg_xact/0000 	modified:   db-data/postmaster.pid 	modified:   db-ui-data/pgadmin4.db 	new file:   db-ui-data/sessions/2bb9e599-86e8-43b0-bdb3-5a3bd80a0032 	new file:   db-ui-data/sessions/6a6333ef-46c2-4775-bdcd-15faec205d72 	modified:   scheduler-data/mage_data/scheduler/.cache/block_action_objects_mapping 	modified:   scheduler-data/mage_data/scheduler/.cache/blocks_to_pipeline_mapping 	modified:   scheduler-data/mage_data/scheduler/.cache/pipeline_details_mapping 	modified:   scheduler-data/mage_data/scheduler/mage-ai.db 	modified:   scheduler-data/scheduler/data_exporters/export_costumers.py 	modified:   scheduler-data/scheduler/io_config.yaml

--- scheduler-data/scheduler/data_exporters/export_costumers.py
+++ scheduler-data/scheduler/data_exporters/export_costumers.py
@@ -1,11 +1,36 @@
 from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.postgres import Postgres
+from mage_ai.data_preparation.shared.secrets import get_secret_value
 from pandas import DataFrame
 from os import path
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
+
+
+def create_qb_table(table_name: str, schema: str):
+    
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    create_query = f"""
+    CREATE SCHEMA IF NOT EXISTS {schema};
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        id TEXT PRIMARY KEY,
+        payload JSONB NOT NULL,
+        ingested_at_utc TIMESTAMPTZ NOT NULL,
+        extract_window_start_utc TIMESTAMPTZ NOT NULL,
+        extract_window_end_utc TIMESTAMPTZ NOT NULL,
+        page_number INT NOT NULL,
+        page_size INT NOT NULL,
+        request_payload TEXT NOT NULL
+    );
+    """
+    with Postgres.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+        print(f"Checking/Creating table: {table_name}...")
+        loader.execute(create_query)
+        print(f"Verified {table_name}.")
 
 
 @data_exporter
@@ -16,16 +41,33 @@ def postgres_data_export(df: DataFrame, **kwargs) -> None:
 
     Docs: https://docs.mage.ai/design/data-loading#postgresql
     """
-    schema_name = 'qb_costumer'  # Specify the name of the schema to export data to
-    table_name = 'your_table_name'  # Specify the name of the table to export data to
+    schema_name = get_secret_value('pg_schema')  # Specify the name of the schema to export data to
+    table_name = 'qb_costumer'  # Specify the name of the table to export data to
+
+    create_qb_table(table_name, schema_name)
+
+    full_table_name = f'{schema}.{table_name}'
+
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
+    print('Beginning data exportation...')
     with Postgres.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
         loader.export(
             df,
             schema_name,
             table_name,
-            index=False,  # Specifies whether to include index in exported table
-            if_exists='replace',  # Specify resolution policy if table name already exists
+            index=False,
+            if_exists='append',  # UPSERT defined below by updating if it exists instead
+            unique_constaints=['id'],
+            unique_conflict_method='UPDATE',
+            allow_reversed_words=True
         )
+    print('Data exported to the database.')
+
+    number_exported = 0
+
+    with Postgres.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+        number_exported = loader.execture('SELECT COUNT(*) FROM {full_table_name}')
+
+    print('Total number of entries in the database: ', number_exported)
